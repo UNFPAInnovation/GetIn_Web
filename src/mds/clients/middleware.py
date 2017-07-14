78828455f3aed62a44461b674c96381c4b1a4ba3
ewinkler <winkler.em@gmail.com>
2017-07-14 18:20:50 +0000

    Fixed auth handler

--- src/mds/clients/middleware.py
+++ src/mds/clients/middleware.py
@@ -2,13 +2,16 @@ from django.conf import settings
 
 class APIKeyAuthenticator(object):
     def process_request(self, request):
-        bearer_token = request.META.get("HTTP_AUTHORIZATION", None)
-        token = None
-        if bearer_token:
-            token = bearer_token.split(' ')[1]
-        # Check authentication token
-        authenticated = False
-        if token and token == settings.API_KEY_UPDATE:
-            authenticated = True
-        setattr(request,'authenticated', authenticated)
+        auth = request.META.get("HTTP_AUTHORIZATION", None)
+        if auth and auth.startswith('Bearer'):
+            authenticated = False
+            token = None
+            try:
+                token_type, token = auth.split()
+            except:
+                pass
+            # Check authentication token
+            if token and token == settings.API_KEY_UPDATE:
+                authenticated = True
+            setattr(request,'authenticated', authenticated)
         return None
