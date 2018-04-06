ac614579389fb76edb9f6df1fa288ab1e130c62e
ewinkler <winkler.em@gmail.com>
2018-04-06 15:35:02 -0400

    Add client crash report model

--- src/mds/clients/models.py
+++ src/mds/clients/models.py
@@ -13,3 +13,9 @@ class Client(models.Model):
     flavor = models.CharField(max_length=32, 
         choices=((x,x) for x in ('vht','midwife')),
         blank=True)
+
+class CrashReport(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    device = models.CharField(max_length=36, blank=True)
+    report = models.FileField(upload_to='reports/', blank=True,)
+    message = models.CharField(max_length=255, blank=True, default="None")
